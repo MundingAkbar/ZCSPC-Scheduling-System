66d677bc828b97fcdabb6ab1d0fe2be81641d492
MundingAkbar <sl201501058@wmsu.edu.ph>
2021-01-27 21:25:01 +0800

    .

---
+++ admin.php
@@ -0,0 +1,93 @@
+
+<?php require_once "nav.php"; ?>
+<div class="container">
+    <div class="card mt-5">
+        <div class="card-header">
+           <span><i class="fas fa-chart-line"></i></span> Dashboard
+        </div>
+        <div class="card-body">
+
+            <div class="row justify-content-center">
+                <div class="col-md-3">
+
+                        <div class="card border-dark count-card">
+                            <div class="card-body">
+                            
+                                <div class="row justify-content-center">
+                                    <div class="col-4">
+                                        <h1><span class="image"><i class="fas fa-user"></i></span></h1>
+                                    </div>
+                                    <div class="col-8">
+                                        <h2 class="count">29</h2>
+                                        <h3>College</h3>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        
+                </div>
+                <div class="col-md-3">
+
+                        <div class="card border-dark count-card">
+                            <div class="card-body">
+                            
+                                <div class="row justify-content-center">
+                                    <div class="col-4">
+                                        <h1><span class="image"><i class="fas fa-user"></i></span></h1>
+                                    </div>
+                                    <div class="col-8">
+                                        <h2 class="count">29</h2>
+                                        <h4>Senior High</h4>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        
+                </div>
+
+            </div>
+
+        </div>
+    </div>
+    <!-- table -->
+    <div class="card mt-3">
+        <div class="card-header">
+            Recently Registered
+        </div>
+        <div class="card-body">
+            <div class="table-responsive">
+            <table class="table">
+                <tr>
+                    <th>#</th>
+                    <th>First Name</th>
+                    <th>Middle Name</th>
+                    <th>Last Name</th>
+                </tr>
+                
+                        <tr>
+                            <td></td>
+                            <td>Ahmad</td>
+                            <td>Tiyuh</td>
+                            <td>Jaalam</td>
+                        </tr>
+                        <tr>
+                            <td></td>
+                            <td>Jarnisa</td>
+                            <td>jala</td>
+                            <td>hussam</td>
+                        </tr>
+                        <tr>
+                            <td></td>
+                            <td>jalaluddin</td>
+                            <td>nishma</td>
+                            <td>nur</td>
+                        </tr>
+                
+            </table>
+             </div>
+        </div>
+    </div>
+    <!-- end of tahble -->
+</div>
+</body>
+</html>
---
+++ student.php
@@ -0,0 +1,16 @@
+
+<?php require_once"nav.php";?><br>
+<div class="container">
+<div class="dropdown">
+  <a class="btn btn-secondary dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-bs-toggle="dropdown" aria-expanded="false">
+    View by
+  </a>
+
+  <ul class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+    <li><a class="dropdown-item" href="#">Action</a></li>
+    <li><a class="dropdown-item" href="#">College</a></li>
+    <li><a class="dropdown-item" href="#">Senior High</a></li>
+  </ul>
+</div>
+<?php require_once "studtable.php";?>
+</div>
